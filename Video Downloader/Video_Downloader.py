bc5c51870d552ef48de73a9f9b6b073b9cc93038
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2023-11-24 10:41:45 +1100

    Add files via upload

--- Video Downloader/Video_Downloader.py
+++ Video Downloader/Video_Downloader.py
@@ -1,7 +1,36 @@
-import requests
-chunk_size = 256
-url = input('Paste the url you wish to download from here: ')
-r = requests.get(url, stream=True)
-with open('Video.mp4', 'wb') as f:
-    for chunk in r.iter_content(chunk_size=chunk_size):
-        f.write(chunk)
+from pytube import YouTube
+from sys import argv
+import os
+
+
+# link must be placed in ''
+link = argv[1] # Takes first arg specified when runing the program
+yt = YouTube(link)
+Thumbnail = yt.thumbnail_url
+Name = yt.title
+
+print('1) Video')
+print('2) Audio')
+Usr_Input = int(input('Would you like to download an Audio or Video file? '))
+
+
+if 1 == Usr_Input:
+
+    yd = yt.streams.get_highest_resolution()
+
+    print(f'Downloading {Name}')
+    yd.download('Downloaded-Content')
+
+    #if os.path.isfile(f'~/Videos/{Name}'):
+    #    print(f'{Name} Downloaded')
+    #else:
+    #    print(f'Error occured while downloading {Name}')
+
+elif 2 == Usr_Input:
+    print(f'Downloading {Name}')
+    yt.streams.get_audio_only(link)
+    
+    if os.path.isfile('Downloaded-Content/{Name}'):
+        print(f'{Name} Downloaded')
+    else:
+        print(f'Error occured while downloading {Name}')
