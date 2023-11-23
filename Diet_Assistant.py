243a2d39bc7a6f038e29820277a2755c4909e0fc
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2023-11-23 21:35:00 +1100

    I broke it

--- Diet_Assistant.py
+++ Diet_Assistant.py
@@ -1,20 +1,25 @@
-import os
-import json
+import os, json
 
-BMR = 1619
+# BMR = 1619
 
 def Check_BMR():
     if os.path.isfile('BMR.json'):
         f = open('BMR.json', 'r')
-        y = f.readline()                               
+        y = f.readline()
+        f.close()                               
         print('You have ' + y + ' calories remaining')
     else:
         print('Error: Log file corrupted or removed')
         exit
 
 def Reset_Daily_BMR():                                          
-    if os.path.isfile('BMR.json'):                              
-        Write_Data(BMR)
+    if os.path.isfile('BMR.json') & os.path.isfile('Goal.json'):
+        f_BMR = open('BMR.json', 'w')
+        f_Goal = open('Goal.json', 'r')
+        Goal = str(f_Goal.readlines())
+        f_BMR.write(json.dumps(int(Goal)))
+        f_BMR.close()
+        f_Goal.close()
         print('Calories Reset')                                 
         print('GOOD LUCK TODAY! : )')
     else:
@@ -29,10 +34,26 @@ def Write_Data(x):
 def Read_Data():
     f = open('BMR.json', 'r')
     x = int(f.readline())
+    f.close()
     print(x)
     Calculate_Cal_Remaining(x)
+    
+    
+def Change_Goal(y):
+    f = open('Goal.json', 'w')
+    f.write(json.dumps(int(y)))
+    f.close()
 
-
+def First_Time_Setup():
+    Goal = int(input('What would you like your daily calorie goal to be? '))
+    Change_Goal(Goal)
+    if os.path.isfile('Goal.json'):
+        f_BMR = open('BMR.json', 'w')
+        f_Goal = open('Goal.json', 'r')
+        Goal = str(f_Goal.readlines())
+        f_BMR.write(json.dumps(int(Goal)))
+        f_BMR.close()
+        f_Goal.close()
 
 def Calculate_Cal_Per_Serve():
     Serves = float(input('How many serves? '))
@@ -41,11 +62,9 @@ def Calculate_Cal_Per_Serve():
 
 def Calculate_Cal_Remaining(data):
     Usr_Input = int(input('How many calories in your last meal? '))
-    if data != 0:
-        Usr_BMR = data - Usr_Input
-    else:
-        Usr_BMR = BMR - Usr_Input
-    print('Total daily calories: ' + str(BMR))
+    Usr_BMR = data - Usr_Input
+    
+    print('Total daily calories: ' + str(Read_Data()))
     print('Remaining calories: ' + str(Usr_BMR))
     Write_Data(Usr_BMR)
 
@@ -55,21 +74,24 @@ print('1) Calculate calories per serve')
 print('2) Calculate calories remaining')
 print('3) Reset daily calore count')
 print('4) Check how many calories you have left')
-print('5) Exit')
+print('5) Fisrt time setup')
+print('6) Exit')
 
 Selection = input('Enter the corresponding number: ')
 if '1' in Selection:
     Calculate_Cal_Per_Serve()
 elif '2' in Selection:
     if os.path.isfile('BMR.json'):
-    	Read_Data()
+        Read_Data()
     else:
-        Write_Data(BMR)
+        Write_Data(Read_Data())
 elif '3' in Selection:
     Reset_Daily_BMR()
 elif '4' in Selection:
     Check_BMR()
 elif '5' in Selection:
+    First_Time_Setup()
+elif '6' in Selection:
     exit
 else:
     print('Error: Input Incorrect')
