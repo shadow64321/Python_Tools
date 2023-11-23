400ed58ff944f194e52bc9b5ae1679ea24531f13
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2023-11-23 23:37:15 +1100

    Installers YAY!

---
+++ Arch-Linux-Installer.sh
@@ -0,0 +1,4 @@
+pacman -S python &
+pacman -S python-pip &
+pip install pyinstaller &
+pyinstaller Diet_Assistant.py --onefile
---
+++ Deb-Linux-Installer.sh
@@ -0,0 +1,4 @@
+sudo apt install python3 &
+sudo apt install python3-pip &
+pip install pyinstaller &
+pyinstaller Diet_Assistant.py --onefile
