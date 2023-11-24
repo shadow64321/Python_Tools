65b41347cb5beb644ffd88eeb9bc7e7e627fa4ee
Elephants_Like_Crayons_Too <73987620+shadow64321@users.noreply.github.com>
2023-11-24 11:52:26 +1100

    Add files via upload

--- Video Downloader/Video_Downloader.py
+++ Video Downloader/Video_Downloader.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 from sys import argv
+import subprocess
 import os
 
 
@@ -17,9 +18,20 @@ Usr_Input = int(input('Would you like to download an Audio or Video file? '))
 if 1 == Usr_Input:
 
     yd = yt.streams.get_highest_resolution()
-
+    # yd = yt.streams.filter(progressive=True).filter(mime_type='video/webm').first()
     print(f'Downloading {Name}')
+    # Get Audio
+    #t=yt.streams.filter(only_audio=True)
+    #downloaded_file = t[0].download('Downloaded-Content/Audio')
+    #base, ext = os.path.splitext(downloaded_file)
+    #new_file = base + '.mp3'
+    #os.rename(downloaded_file, new_file)
+    # Get Video
     yd.download('Downloaded-Content/Videos')
+    #Combine
+    #command = 'vlc ' + Name + '.mp4 ' + new_file + ' --sout="#gather:std{access=file, mux=ts, dst=final.mp4}" --sout-keep'
+    #os.system(command)
+
     print(f'Downloaded {Name}')
 
     #if os.path.isfile(f'~/Videos/{Name}.mp4'):
